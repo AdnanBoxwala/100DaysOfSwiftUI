e57637c44bef59a48b6877e5bb1e54bab5cd5326
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2023-09-16 23:40:53 +0200

    Day 23: Project 3 - Part 1 - ViewsAndModifiers

---
+++ 01_Starting_SwiftUI/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Adnan Boxwala on 16.09.23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
