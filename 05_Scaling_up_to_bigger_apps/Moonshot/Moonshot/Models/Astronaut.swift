91867b0f450c374f839f5a991f2f2a485943e19a
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2023-10-05 21:23:14 +0200

    Day 41: Project 8 - Moonshot
    Learnt about GeometryReader, ScrollView, Codable, JSONDecoder, Generics

---
+++ 05_Scaling_up_to_bigger_apps/Moonshot/Moonshot/Models/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Adnan Boxwala on 05.10.23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ 05_Scaling_up_to_bigger_apps/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Adnan Boxwala on 04.10.23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
