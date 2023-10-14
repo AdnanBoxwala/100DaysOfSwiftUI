eb7a6d67f94df3f3b5b73f96b0326ca9d04a3ec0
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2023-10-14 23:48:24 +0200

    Day 49: Project 10 - Codable conformance @Published

---
+++ 07_Focus_on_data/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Adnan Boxwala on 14.10.23.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Adnan Boxwala"
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 07_Focus_on_data/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Adnan Boxwala on 14.10.23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
