4188e378d3534788778d0da7629952d932da55a0
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2023-10-17 22:10:22 +0200

    Day 53: Project 11 Part 1 - Custom component with @Binding

---
+++ 07_Focus_on_data/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Adnan Boxwala on 17.10.23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
