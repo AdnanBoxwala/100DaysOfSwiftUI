95c1d7c140120cb1250bbb664194247f4b9cde04
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2023-10-25 23:06:17 +0200

    Day 57: Project 12 Part 1 - Creating NSManagedObject subclasses
    CoreData automatically creates a class for your entity. This can also be created manually using editor -> create NSManagedObject Subclass.
    To avoid nil coalescing in code, add computed vars around your property in the subclass, so that nil coalescing is done only in one place.

---
+++ 07_Focus_on_data/CoreDataProject/CoreDataProject/Models/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Adnan Boxwala on 25.10.23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
