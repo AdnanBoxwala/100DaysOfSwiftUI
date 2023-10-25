f0d990715bb6f97910b720977ac264fcde0d26ba
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2023-10-25 23:24:44 +0200

    Day 57: Project 12 Part 1 - Conditional saving of NSManagedObjectContext

--- 07_Focus_on_data/CoreDataProject/CoreDataProject/Views/ContentView.swift
+++ 07_Focus_on_data/CoreDataProject/CoreDataProject/Views/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Save") {
+            if moc.hasChanges {
+                try? moc.save()
+            }
         }
-        .padding()
     }
 }
 
