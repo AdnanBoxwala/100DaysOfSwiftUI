168bd6941a6f7704d3bd57f6a9cdcfc06a13f1e6
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2023-10-28 16:36:05 +0200

    Day 58: Project 12 Part 2 - @FetchRequest using NSPredicate

--- 07_Focus_on_data/CoreDataProject/CoreDataProject/Views/ContentView.swift
+++ 07_Focus_on_data/CoreDataProject/CoreDataProject/Views/ContentView.swift
@@ -5,30 +5,58 @@
 //  Created by Adnan Boxwala on 25.10.23.
 //
 
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
+//    @FetchRequest(sortDescriptors: [], predicate: nil) var ships: FetchedResults<Ship>
+//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == 'Star Wars'")) var ships: FetchedResults<Ship>
     
-    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    // MARK: TIP -  %@
+    // Use %@ for readability. Useful when the matching argument has quotes
+//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == %@", "Star Wars")) var ships: FetchedResults<Ship>
+//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name < %@", "F")) var ships: FetchedResults<Ship>
+//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe IN %@", ["Aliens", "Firefly", "Star Trek"])) var ships: FetchedResults<Ship>
+//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name BEGINSWITH %@", "E")) var ships: FetchedResults<Ship>
+    
+    // MARK: TIP -  [c]
+    // Arguments are case-sensitive. Use [c] to make it case-insensitive
+//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
+//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name CONTAINS[c] %@", "e")) var ships: FetchedResults<Ship>
+    
+    // MARK: TIP -  NOT
+    // Adding NOT returns inverse
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
+    
+    // MARK: TIP -  Combine predicates
+    // There is also an ADD keyword to combine predicates
+    // Another option is to use NSCompound from CoreData to create an NSPredicate from multiple smaller predicates
     
     var body: some View {
         VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
             }
             
-            Button("Add") {
-                let wizard = Wizard(context: moc)
-                wizard.name = "Harry Potter"
-            }
-            
-            Button("Save") {
-                do {
-                    try moc.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
+            Button("Add Examples") {
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: moc)
+                ship3.name = "Millenium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? moc.save()
             }
         }
     }
