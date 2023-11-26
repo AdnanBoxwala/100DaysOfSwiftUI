ae0c6567ff535483401701a73005dc37245053ba
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2023-11-26 12:39:50 +0100

    Day 63: Project 13 Part 2 - CoreImage in SwiftUI
    center property for CIFilter.twirlDistortion() may contain a bug in iOS 17: whole width and height of image must be given for the distortion to actually happen at the center.

--- 09_Views_and_view_controllers/Instafilter/Instafilter/ContentView.swift
+++ 09_Views_and_view_controllers/Instafilter/Instafilter/ContentView.swift
@@ -5,26 +5,41 @@
 //  Created by Adnan Boxwala on 26.11.23.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
     
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .frame(width: 300, height: 300)
-                .background(backgroundColor)
-                .onTapGesture { showingConfirmation = true }
-                .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                    Button("Red") { backgroundColor = .red }
-                    Button("Green") { backgroundColor = .green }
-                    Button("Blue") { backgroundColor = .blue }
-                    Button("Cancel", role: .cancel) { }
-                } message: {
-                    Text("Select a new color")
-                }
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+        currentFilter.center = CGPoint(x: inputImage.size.width, y: inputImage.size.height)
+        
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(amount, forKey: kCIInputIntensityKey)}
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)}
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)}
+                
+        guard let outputImage = currentFilter.outputImage else { return }
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
         }
     }
 }
