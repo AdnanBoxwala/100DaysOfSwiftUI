15e949ff2d58344b4c7146f8a9117a9fe4609fba
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2023-12-11 22:26:44 +0100

    Day 69: Project 14 Part 2: MapKit SwiftUI
    for iOS17, old MapKit API is deprecated.
    Tried using newer API.
    waiting for course to be updated

--- 09_Views_and_view_controllers/BucketList/BucketList/ContentView.swift
+++ 09_Views_and_view_controllers/BucketList/BucketList/ContentView.swift
@@ -5,44 +5,35 @@
 //  Created by Adnan Boxwala on 10.12.23.
 //
 
+import MapKit
 import SwiftUI
 
-enum LoadingState {
-    case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("success...")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("failed...")
-    }
-}
-
 struct ContentView: View {
-    var loadingState = LoadingState.failed
+    @State private var position: MapCameraPosition = .region(MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    )
     
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
+        NavigationStack {
+            Map(position: $position) {
+                Marker("Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141))
+                
+                Annotation("Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076), anchor: .center) {
+                    NavigationLink {
+                        Text("Tower of London")
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                    }
+                }
+            }
+            .navigationTitle("London Explorer")
         }
     }
 }
+
 
 #Preview {
     ContentView()
