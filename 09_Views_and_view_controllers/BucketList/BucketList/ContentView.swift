a80b47106939894b9febdeddd44598cfe728928e
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2024-01-14 13:18:09 +0100

    DayDay 69: Project 14 Part 2 - Biometric authentication

--- 09_Views_and_view_controllers/BucketList/BucketList/ContentView.swift
+++ 09_Views_and_view_controllers/BucketList/BucketList/ContentView.swift
@@ -5,31 +5,40 @@
 //  Created by Adnan Boxwala on 10.12.23.
 //
 
-import MapKit
+import LocalAuthentication
 import SwiftUI
 
-struct Location: Identifiable {
-    let id = UUID()
-    let name: String
-    let coordinate: CLLocationCoordinate2D
-}
-
 struct ContentView: View {
-    let locations = [
-        Location(name: "buckingham palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "tower of london", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
+    @State private var isunlocked = false
     
     var body: some View {
         VStack {
-            MapReader { proxy in
-                Map()
-                    .onTapGesture { position in
-                        if let coordinate = proxy.convert(position, from: .local) {
-                            print(coordinate)
-                        }
-                    }
+            if isunlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
             }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isunlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+            print(error?.localizedDescription ?? "no biometrics")
         }
     }
 }
