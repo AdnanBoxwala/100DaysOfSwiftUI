11cef25a7b27630082288a4142246d4534c1916d
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2024-01-14 12:19:07 +0100

    Day 69: Project 14 Part 2 - Integrating MapKit
    how to integrate map, add mapstyle, interactive modes, initial position

--- 09_Views_and_view_controllers/BucketList/BucketList/ContentView.swift
+++ 09_Views_and_view_controllers/BucketList/BucketList/ContentView.swift
@@ -5,23 +5,37 @@
 //  Created by Adnan Boxwala on 10.12.23.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+                           span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+    )
     
     var body: some View {
-        Button("Read and Write") {
-            let content = "Test Message"
-            let filename = "message.txt"
+        VStack {
+            Map(position: $position)
+                .mapStyle(.hybrid(elevation: .realistic))
+                .onMapCameraChange(frequency: .continuous) { context in
+                    print(context.region)
+                }
             
-            FileManager.default.encode(content, to: filename) { error in
-                print(error.localizedDescription)
-            }
-            
-            if let readContent: String = FileManager.default.decode(from: filename, completionHandler: { error in
-                print(error.localizedDescription)
-            }) {
-                print(readContent)
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+                                           span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+                    )
+                }
+                
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+                                           span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+                    )
+                }
             }
         }
     }
