b80ef4245ace61f45a6b29c103bcd90ae050be1c
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2024-01-14 16:21:21 +0100

    Day 70: Project 14 Part 3 - Improving annotations

--- 09_Views_and_view_controllers/BucketList/BucketList/ContentView.swift
+++ 09_Views_and_view_controllers/BucketList/BucketList/ContentView.swift
@@ -20,13 +20,20 @@ struct ContentView: View {
         MapReader { proxy in
             Map(initialPosition: startPosition) {
                 ForEach(locations) { location in
-                    Marker(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitutde))
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                    }
                 }
             }
             .mapStyle(.hybrid(elevation: .realistic))
             .onTapGesture { position in
                 if let coordinate = proxy.convert(position, from: .local) {
-                    let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitutde: coordinate.longitude)
+                    let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
                     locations.append(newLocation)
                 }
             }
--- 09_Views_and_view_controllers/BucketList/BucketList/Location.swift
+++ 09_Views_and_view_controllers/BucketList/BucketList/Location.swift
@@ -6,11 +6,24 @@
 //
 
 import Foundation
+import MapKit
 
 struct Location: Codable, Equatable, Identifiable {
     let id: UUID
     var name: String
     var description: String
     var latitude: Double
-    var longitutde: Double
+    var longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    #if DEBUG
+    static let example = Location(id: UUID(), name: "Ingolstadt", description: "Home to Audi AG.", latitude: 48.7667, longitude: 11.4226)
+    #endif
+    
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
