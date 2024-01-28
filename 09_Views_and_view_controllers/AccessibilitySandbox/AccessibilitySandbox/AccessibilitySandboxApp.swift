0a98e9056948c2561b87f67e6870d4b23debf5e5
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2024-01-28 21:50:15 +0100

    Day 74: Project 15 Part 1 - Adding useful voice labels

---
+++ 09_Views_and_view_controllers/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Adnan Boxwala on 28.01.24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 09_Views_and_view_controllers/AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  AccessibilitySandbox
+//
+//  Created by Adnan Boxwala on 28.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    var body: some View {
+        Button {
+            selectedPicture = Int.random(in: 0...3)
+        } label: {
+            Image(pictures[selectedPicture])
+                .resizable()
+                .scaledToFit()
+        }
+        .accessibilityLabel(labels[selectedPicture])
+//        .accessibilityAddTraits(.isButton)
+//        .accessibilityRemoveTraits(.isImage)
+    }
+}
+
+#Preview {
+    ContentView()
+}
