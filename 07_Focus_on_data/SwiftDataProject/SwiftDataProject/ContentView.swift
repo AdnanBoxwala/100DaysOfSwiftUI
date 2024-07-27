bd3f1b12e9ea8c17508bf66e857d7d00c9796659
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2024-07-28 00:23:12 +0200

    Day 53: Project 11 Part 1: @Binding iOS17

---
+++ 07_Focus_on_data/SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  SwiftDataProject
+//
+//  Created by Adnan Boxwala on 28.07.24.
+//
+
+import SwiftUI
+
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.6)]
+    
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom))
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct ContentView: View {
+    @State private var rememberMe = false
+    
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 07_Focus_on_data/SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Adnan Boxwala on 28.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
