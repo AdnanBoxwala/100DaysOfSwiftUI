f2abfbade13fab967860d44cd142a54870433d78
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2024-07-27 13:31:35 +0200

    Day 79: Project 16 Part 1: TabView, tabItem, tag

--- 11_Controlling_UI_flow/HotProspects/HotProspects/ContentView.swift
+++ 11_Controlling_UI_flow/HotProspects/HotProspects/ContentView.swift
@@ -8,26 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    let users = ["Tohru", "Yuki", "Kyo", "Momiji"]
-//    Use optional string for single selection
-//    @State private var selection: String?
-    @State private var selection = Set<String>()
+    @State private var selectedTab = "One"
     
     var body: some View {
-        List(users, id: \.self, selection: $selection) { user in
-            Text(user)
+        TabView(selection: $selectedTab) {
+            Button("Show Tab 2") {
+                selectedTab = "Two"
+            }
+            .tabItem {
+                Label("One", systemImage: "star")
+            }
+            .tag("One")
+            
+            Button("Show Tab 1") {
+                selectedTab = "One"
+            }
+            .tabItem {
+                Label("Two", systemImage: "circle")
+            }
+            .tag("Two")
         }
-        
-//        if let selection {
-        if selection.isEmpty == false {
-//            Text("You selected \(selection)")
-            Text("You selected \(selection.formatted())")
-        }
-        
-        // Multiselection can be enabled with gesture
-        // Right swip with two fingers
-        // Not obvious, so use EditButton
-        EditButton()
     }
 }
 
